3fee74ad3a4ca31bb2ed8f68bff26bba299d54b6
baudin <baudin@b5ecbf09-c971-4885-9eb9-8da14162421c>
2011-11-29 14:16:28 +0000

    * Added Copyright notices in the gateways.
    git-svn-id: http://svn.forge.scilab.org/lowdisc@164 b5ecbf09-c971-4885-9eb9-8da14162421c

--- sci_gateway/cpp/sci_lowdisc_niedfskipget.cpp
+++ sci_gateway/cpp/sci_lowdisc_niedfskipget.cpp
@@ -1,6 +1,9 @@
 
 // Copyright (C) 2008 - INRIA - Michael Baudin
 // Copyright (C) 2009-2010 - Digiteo - Michael Baudin
+//
+// This file must be used under the terms of the GNU Lesser General Public License license :
+// http://www.gnu.org/copyleft/lesser.html
 
 extern "C" {
 #include "stack-c.h" 
