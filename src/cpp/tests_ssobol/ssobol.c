450ce57c2fcd660aad13db75d1c68813193008a0
baudin <baudin@b5ecbf09-c971-4885-9eb9-8da14162421c>
2013-04-06 20:54:14 +0000

    Draft v2.
    git-svn-id: http://svn.forge.scilab.org/lowdisc@223 b5ecbf09-c971-4885-9eb9-8da14162421c

--- src/cpp/tests_ssobol/ssobol.c
+++ src/cpp/tests_ssobol/ssobol.c
@@ -17,105 +17,23 @@ int exor_(int *iin, int *jin);
 int genscrml_(int *maxd, int *lsm, int *shift);
 int genscrmu_(int *usm, int *ushift);
 double uni_(void);
+int lbit_bits(int a, int b, int len);
 
-static int ssobol_poly[39] = {3, 7, 11, 13, 19, 25, 37, 59, 47, 61, 55, 41, 67, 97, 91, 
+// Static variables
+static int ssobol_poly[39] = {
+	3, 7, 11, 13, 19, 25, 37, 59, 47, 61, 55, 41, 67, 97, 91, 
 	109, 103, 115, 131, 193, 137, 145, 143, 241, 157, 185, 167, 229, 
-	171, 213, 191, 253, 203, 211, 239, 247, 285, 369, 299};
-
-/* Common Block Declarations */
-
-struct sobdat_1_ {
-	int poly[39], vinit[312]	/* was [39][8] */;
+	171, 213, 191, 253, 203, 211, 239, 247, 285, 369, 299
 };
 
-#define sobdat_1 (*(struct sobdat_1_ *) &sobdat_)
-
-union {
-	struct {
-		int s, maxcol, sv[1240]	/* was [40][31] */, count, lastq[40];
-		double recipd;
-	} _1;
-	struct {
-		int s, maxcol;
-	} _2;
-} sobol_;
-
-#define sobol_1 (sobol_._1)
-#define sobol_2 (sobol_._2)
-
-/* Initialized data */
-
-struct {
-	int e_1[78];
-	int fill_2[1];
-	int e_3[38];
-	int fill_4[2];
-	int e_5[37];
-	int fill_6[4];
-	int e_7[35];
-	int fill_8[6];
-	int e_9[33];
-	int fill_10[12];
-	int e_11[27];
-	int fill_12[18];
-	int e_13[21];
-	int fill_14[36];
-	int e_15[3];
-} sobdat_ = { 3, 7, 11, 13, 19, 25, 37, 59, 47, 61, 55, 41, 67, 97, 91, 
-	109, 103, 115, 131, 193, 137, 145, 143, 241, 157, 185, 167, 229, 
-	171, 213, 191, 253, 203, 211, 239, 247, 285, 369, 299, 1, 1, 1, 1,
-	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, {0}, 1, 3, 1, 3, 1, 3, 3, 
-	1, 3, 1, 3, 1, 3, 1, 1, 3, 1, 3, 1, 3, 1, 3, 3, 1, 3, 1, 3, 1, 3, 
-	1, 1, 3, 1, 3, 1, 3, 1, 3, {0}, 7, 5, 1, 3, 3, 7, 5, 5, 7, 7, 1, 
-	3, 3, 7, 5, 1, 1, 5, 3, 3, 1, 7, 5, 1, 3, 3, 7, 5, 1, 1, 5, 7, 7, 
-	5, 1, 3, 3, {0}, 1, 7, 9, 13, 11, 1, 3, 7, 9, 5, 13, 13, 11, 3, 
-	15, 5, 3, 15, 7, 9, 13, 9, 1, 11, 7, 5, 15, 1, 15, 11, 5, 3, 1, 7,
-	9, {0}, 9, 3, 27, 15, 29, 21, 23, 19, 11, 25, 7, 13, 17, 1, 25, 
-	29, 3, 31, 11, 5, 23, 27, 19, 21, 5, 1, 17, 13, 7, 15, 9, 31, 9, {0}, 37, 33, 7, 5, 11, 39, 63, 27, 17, 15, 23, 29, 3, 21, 13, 31, 
-	25, 9, 49, 33, 19, 29, 11, 19, 27, 15, 25, {0}, 13, 33, 115, 41, 
-	79, 17, 29, 119, 75, 73, 105, 7, 59, 65, 21, 3, 113, 61, 89, 45, 
-	107, {0}, 7, 23, 39 };
-
-
-/* Table of constant values */
-
-static double c_b15 = 2.f;
-
-/* Subroutine */ int bdsobl_(void)
-{
-	return 0;
-} /* bdsobl_ */
-
-double pow_ri(double *ap, int *bp)
-{
-	double pow, x;
-	int n;
-	unsigned long u;
-
-	pow = 1;
-	x = *ap;
-	n = *bp;
-
-	if(n != 0)
-	{
-		if(n < 0)
-		{
-			n = -n;
-			x = 1/x;
-		}
-		for(u = n; ; )
-		{
-			if(u & 01)
-				pow *= x;
-			if(u >>= 1)
-				x *= x;
-			else
-				break;
-		}
-	}
-	return(pow);
-}
+static int ssobol_vinit[40][8];
+static double ssobol_recipd;
+static int ssobol_lastq[40];
+static int ssobol_maxcol;
+static int ssobol_count;
+static int ssobol_s;
+static int ssobol_sv[40][31];
+static int ssobol_tau[13] = { 0,0,1,3,5,8,11,15,19,23,27,31,35 };
 
 
 /*     INITIALIZES LABELLED COMMON /SOBDAT/ */
@@ -140,28 +58,18 @@ double pow_ri(double *ap, int *bp)
 /*     VALUES ARE NEEDED :  THESE ARE THE VALUES GIVEN HERE. */
 /*     SUBSEQUENT VALUES ARE CALCULATED IN "INSOBL". */
 
-
-
-
-
-/* Subroutine */ int inssobl_(int *flag__, int *dimen, int *
-	atmost, int *taus, double *quasi, int *maxd, int *
-	iflag)
+int ssobol_startup(int dimen, int atmost, int *taus, double *quasi, int *maxd, int *iflag)
 {
 	/* Initialized data */
 
-	static int tau[13] = { 0,0,1,3,5,8,11,15,19,23,27,31,35 };
 
 	/* System generated locals */
-	int i__1, i__2, i__3, i__4;
+	int i__4;
 	double r__1;
 
-	/* Builtin functions */
-	double pow_ri(double *, int *);
-	int lbit_bits(int, int, int);
-
 	/* Local variables */
-	static int i__, j, k, l, m, p, v[1240]	/* was [40][31] */;
+	static int i, j, k, l, m, p;
+	static int v[1240]	/* was [40][31] */;
 	static double ll;
 	static double pp;
 	static int tv[38440]	/* was [40][31][31] */, lsm[1240]	/* was [40][31] */, usm[961]	/* was [31][31] */;
@@ -223,55 +131,62 @@ double pow_ri(double *ap, int *bp)
 
 	/* Parameter adjustments */
 	--quasi;
-	--flag__;
 
 	/* Function Body */
 
 	/*     CHECK PARAMETERS */
-
-	sobol_1.s = *dimen;
-	flag__[1] = sobol_1.s >= 1 && sobol_1.s <= 40;
-	flag__[2] = *atmost > 0 && *atmost < 1073741824;
-	if (! (flag__[1] && flag__[2])) {
-		return 0;
+	ssobol_s = dimen;
+	if (ssobol_s < 1 || ssobol_s > 40) {
+		printf("ssobol_next : wrong dimension : %d (must be in [1,40]).",ssobol_s);
+		exit(-1);
 	}
-	if (sobol_1.s <= 13) {
-		*taus = tau[sobol_1.s - 1];
+	if (atmost <= 0 || atmost >= 1073741824) {
+		printf("ssobol_next : wrong number of calls : %d (must be in [1,1073741823]).",atmost);
+		exit(-1);
+	}
+	if (ssobol_s <= 13) {
+		*taus = ssobol_tau[ssobol_s - 1];
 	} else {
 		*taus = -1;
 		/*     RETURN A DUMMY VALUE TO THE CALLING PROGRAM */
 	}
 
+	// Initialise ssobol_vinit;
+	for (j = 0; j < 8; j++)
+	{
+		for (i = 1; i < 40; i++) 
+		{
+			ssobol_vinit[i][j] = 0;
+		}
+	}
+#include "ssobol_poly.h"
 
 	/*     FIND NUMBER OF BITS IN ATMOST */
 
-	i__ = *atmost;
-	sobol_1.maxcol = 0;
+	i = atmost;
+	ssobol_maxcol = 0;
 L10:
-	++sobol_1.maxcol;
-	i__ /= 2;
-	if (i__ > 0) {
+	++ssobol_maxcol;
+	i /= 2;
+	if (i > 0) {
 		goto L10;
 	}
 
 	/*     INITIALIZE ROW 1 OF V */
 
-	i__1 = sobol_1.maxcol;
-	for (i__ = 1; i__ <= i__1; ++i__) {
-		/* L20: */
-		v[i__ * 40 - 40] = 1;
+	for (i = 1; i <= ssobol_maxcol; ++i) {
+		v[i * 40 - 40] = 1;
 	}
 
 	/*     INITIALIZE REMAINING ROWS OF V */
 
-	i__1 = sobol_1.s;
-	for (i__ = 2; i__ <= i__1; ++i__) {
+	for (i = 2; i <= ssobol_s; ++i) {
 
 		/*     THE BIT PATTERN OF POLYNOMIAL I GIVES ITS FORM */
 		/*     (SEE COMMENTS TO "BDSOBL") */
 		/*     FIND DEGREE OF POLYNOMIAL I FROM BINARY ENCODING */
 
-		j = ssobol_poly[i__ - 2];
+		j = ssobol_poly[i - 2];
 		m = 0;
 L30:
 		j /= 2;
@@ -283,137 +198,105 @@ L30:
 		/*     WE EXPAND THIS BIT PATTERN TO SEPARATE COMPONENTS */
 		/*     OF THE LOGICAL ARRAY INCLUD. */
 
-		j = ssobol_poly[i__ - 2];
+		j = ssobol_poly[i - 2];
 		for (k = m; k >= 1; --k) {
 			includ[k - 1] = j % 2 == 1;
 			j /= 2;
-			/* L40: */
 		}
 
 		/*     THE LEADING ELEMENTS OF ROW I COME FROM VINIT */
 
-		i__2 = m;
-		for (j = 1; j <= i__2; ++j) {
-			v[i__ + j * 40 - 41] = sobdat_1.vinit[i__ + j * 39 - 41];
-			/* L50: */
+		for (j = 1; j <= m; ++j) {
+			v[i + j * 40 - 41] = ssobol_vinit[i-1][j-1];
 		}
 
 		/*     CALCULATE REMAINING ELEMENTS OF ROW I AS EXPLAINED */
 		/*     IN BRATLEY AND FOX, SECTION 2 */
 
-		i__2 = sobol_1.maxcol;
-		for (j = m + 1; j <= i__2; ++j) {
-			newv = v[i__ + (j - m) * 40 - 41];
+		for (j = m + 1; j <= ssobol_maxcol; ++j) {
+			newv = v[i + (j - m) * 40 - 41];
 			l = 1;
-			i__3 = m;
-			for (k = 1; k <= i__3; ++k) {
+			for (k = 1; k <= m; ++k) {
 				l <<= 1;
 				if (includ[k - 1]) {
-					i__4 = l * v[i__ + (j - k) * 40 - 41];
+					i__4 = l * v[i + (j - k) * 40 - 41];
 					newv = exor_(&newv, &i__4);
 				}
-
 				/*     IF A FULL-WORD EXCLUSIVE-OR, SAY .XOR., IS AVAILABLE, */
 				/*     THEN REPLACE THE PRECEDING STATEMENT BY */
-
 				/*         IF (INCLUD(K)) NEWV = NEWV .XOR. (L * V(I, J-K)) */
-
 				/*     TO GET A FASTER, EXTENDED FORTRAN PROGRAM */
-
-				/* L60: */
 			}
-			v[i__ + j * 40 - 41] = newv;
-			/* L70: */
+			v[i + j * 40 - 41] = newv;
 		}
-
-		/* L100: */
 	}
 
 	/*     MULTIPLY COLUMNS OF V BY APPROPRIATE POWER OF 2 */
 
 	l = 1;
-	for (j = sobol_1.maxcol - 1; j >= 1; --j) {
+	for (j = ssobol_maxcol - 1; j >= 1; --j) {
 		l <<= 1;
-		i__1 = sobol_1.s;
-		for (i__ = 1; i__ <= i__1; ++i__) {
-			v[i__ + j * 40 - 41] *= l;
-			/* L110: */
+		for (i = 1; i <= ssobol_s; ++i) {
+			v[i + j * 40 - 41] *= l;
 		}
-		/* L120: */
 	}
 
 	/* COMPUTING GENERATOR MATRICES OF USER CHOICE */
 
 	if (*iflag == 0) {
-		i__1 = sobol_1.s;
-		for (i__ = 1; i__ <= i__1; ++i__) {
-			i__2 = sobol_1.maxcol;
-			for (j = 1; j <= i__2; ++j) {
-				sobol_1.sv[i__ + j * 40 - 41] = v[i__ + j * 40 - 41];
-				/* L140: */
+		for (i = 1; i <= ssobol_s; ++i) {
+			for (j = 1; j <= ssobol_maxcol; ++j) {
+				ssobol_sv[i-1][j-1] = v[i + j * 40 - 41];
 			}
-			shift[i__ - 1] = 0;
-			/* L130: */
+			shift[i - 1] = 0;
 		}
-		ll = pow_ri(&c_b15, &sobol_1.maxcol);
+		ll = pow(2.0, ssobol_maxcol);
 	} else {
 		if (*iflag == 1 || *iflag == 3) {
 			genscrml_(maxd, lsm, shift);
-			i__1 = sobol_1.s;
-			for (i__ = 1; i__ <= i__1; ++i__) {
-				i__2 = sobol_1.maxcol;
-				for (j = 1; j <= i__2; ++j) {
+			for (i = 1; i <= ssobol_s; ++i) {
+				for (j = 1; j <= ssobol_maxcol; ++j) {
 					l = 1;
 					temp2 = 0;
 					for (p = *maxd; p >= 1; --p) {
 						temp1 = 0;
-						i__3 = sobol_1.maxcol;
-						for (k = 1; k <= i__3; ++k) {
-							temp1 += lbit_bits(lsm[i__ + p * 40 - 41], k - 1, 
-								(int)1) * lbit_bits(v[i__ + j * 40 - 
+						for (k = 1; k <= ssobol_maxcol; ++k) {
+							temp1 += lbit_bits(lsm[i + p * 40 - 41], k - 1, 
+								(int)1) * lbit_bits(v[i + j * 40 - 
 								41], k - 1, (int)1);
-							/* L260: */
 						}
 						temp1 %= 2;
 						temp2 += temp1 * l;
 						l <<= 1;
-						/* L250: */
 					}
-					sobol_1.sv[i__ + j * 40 - 41] = temp2;
-					/* L240: */
+					ssobol_sv[i-1][j-1] = temp2;
 				}
-				/* L230: */
 			}
-			ll = pow_ri(&c_b15, maxd);
+			ll = pow(2.0, *maxd);
 		}
 		if (*iflag == 2 || *iflag == 3) {
 			genscrmu_(usm, ushift);
 			if (*iflag == 2) {
-				maxx = sobol_1.maxcol;
+				maxx = ssobol_maxcol;
 			} else {
 				maxx = *maxd;
 			}
-			i__1 = sobol_1.s;
-			for (i__ = 1; i__ <= i__1; ++i__) {
-				i__2 = sobol_1.maxcol;
-				for (j = 1; j <= i__2; ++j) {
+			for (i = 1; i <= ssobol_s; ++i) {
+				for (j = 1; j <= ssobol_maxcol; ++j) {
 					p = maxx;
-					i__3 = maxx;
-					for (k = 1; k <= i__3; ++k) {
+					for (k = 1; k <= maxx; ++k) {
 						if (*iflag == 2) {
-							tv[i__ + (p + j * 31) * 40 - 1281] = lbit_bits(v[
-								i__ + j * 40 - 41], k - 1, (int)1);
+							tv[i + (p + j * 31) * 40 - 1281] = lbit_bits(v[
+								i + j * 40 - 41], k - 1, (int)1);
 						} else {
-							tv[i__ + (p + j * 31) * 40 - 1281] = lbit_bits(
-								sobol_1.sv[i__ + j * 40 - 41], k - 1, (
+							tv[i + (p + j * 31) * 40 - 1281] = lbit_bits(
+								ssobol_sv[i-1][j-1], k - 1, (
 								int)1);
 						}
 						--p;
-						/* L350: */
 					}
-					/* L340: */
 				}
-				r__1 = (double) sobol_1.maxcol;
+				r__1 = (double) ssobol_maxcol;
 				for (pp = 1.f; pp <= r__1; pp += 1.f) {
 					temp2 = 0;
 					temp4 = 0;
@@ -421,15 +304,13 @@ L30:
 					for (j = maxx; j >= 1; --j) {
 						temp1 = 0;
 						temp3 = 0;
-						i__2 = sobol_1.maxcol;
-						for (p = 1; p <= i__2; ++p) {
-							temp1 += tv[i__ + (j + p * 31) * 40 - 1281] * usm[
+						for (p = 1; p <= ssobol_maxcol; ++p) {
+							temp1 += tv[i + (j + p * 31) * 40 - 1281] * usm[
 								p + (int) pp * 31 - 32];
 								if (pp == 1.f) {
-									temp3 += tv[i__ + (j + p * 31) * 40 - 1281] * 
+									temp3 += tv[i + (j + p * 31) * 40 - 1281] * 
 										ushift[p - 1];
 								}
-								/* L370: */
 						}
 						temp1 %= 2;
 						temp2 += temp1 * l;
@@ -438,47 +319,39 @@ L30:
 							temp4 += temp3 * l;
 						}
 						l <<= 1;
-						/* L360: */
 					}
-					sobol_1.sv[i__ + (int) pp * 40 - 41] = temp2;
+					ssobol_sv[i-1][(int) pp-1] = temp2;
 					if (pp == 1.f) {
 						if (*iflag == 3) {
-							shift[i__ - 1] = exor_(&temp4, &shift[i__ - 1]);
+							shift[i - 1] = exor_(&temp4, &shift[i - 1]);
 						} else {
-							shift[i__ - 1] = temp4;
+							shift[i - 1] = temp4;
 						}
 					}
-					/* L341: */
 				}
-				/* L330: */
 			}
-			ll = pow_ri(&c_b15, &maxx);
+			ll = pow(2.0, maxx);
 		}
 	}
 
 	/*     RECIPD IS 1/(COMMON DENOMINATOR OF THE ELEMENTS IN V) */
 
-	sobol_1.recipd = 1.f / ll;
+	ssobol_recipd = 1.0 / ll;
 
 	/*     SET UP FIRST VECTOR AND VALUES FOR "GOSOBL" */
 
-	sobol_1.count = 0;
-	i__1 = sobol_1.s;
-	for (i__ = 1; i__ <= i__1; ++i__) {
-		sobol_1.lastq[i__ - 1] = shift[i__ - 1];
-		quasi[i__] = sobol_1.lastq[i__ - 1] * sobol_1.recipd;
-		/* L400: */
+	ssobol_count = 0;
+	for (i = 1; i <= ssobol_s; ++i) {
+		ssobol_lastq[i - 1] = shift[i - 1];
+		quasi[i] = ssobol_lastq[i - 1] * ssobol_recipd;
 	}
 	return 0;
-} /* inssobl_ */
+}
 
 /* Subroutine */ int genscrml_(int *maxd, int *lsm, int *shift)
 {
-	/* System generated locals */
-	int i__1;
-
 	/* Local variables */
-	static int i__, j, l, p, ll;
+	static int i, j, l, p, ll;
 	static int temp, stemp;
 
 	/*     GENERATING LOWER TRIANULAR SCRMABLING MATRICES AND SHIFT VECTORS. */
@@ -493,42 +366,35 @@ L30:
 	lsm -= 41;
 
 	/* Function Body */
-	i__1 = sobol_2.s;
-	for (p = 1; p <= i__1; ++p) {
+	for (p = 1; p <= ssobol_s; ++p) {
 		shift[p] = 0;
 		l = 1;
-		for (i__ = *maxd; i__ >= 1; --i__) {
-			lsm[p + i__ * 40] = 0;
+		for (i = *maxd; i >= 1; --i) {
+			lsm[p + i * 40] = 0;
 			stemp = (int) (uni_() * 1e3f) % 2;
 			shift[p] += stemp * l;
 			l <<= 1;
 			ll = 1;
-			for (j = sobol_2.maxcol; j >= 1; --j) {
-				if (j == i__) {
+			for (j = ssobol_maxcol; j >= 1; --j) {
+				if (j == i) {
 					temp = 1;
-				} else if (j < i__) {
+				} else if (j < i) {
 					temp = (int) (uni_() * 1e3f) % 2;
 				} else {
 					temp = 0;
 				}
-				lsm[p + i__ * 40] += temp * ll;
+				lsm[p + i * 40] += temp * ll;
 				ll <<= 1;
-				/* L30: */
 			}
-			/* L20: */
 		}
-		/* L10: */
 	}
 	return 0;
 } /* genscrml_ */
 
 /* Subroutine */ int genscrmu_(int *usm, int *ushift)
 {
-	/* System generated locals */
-	int i__1, i__2;
-
 	/* Local variables */
-	static int i__, j;
+	static int i, j;
 	static int temp, stemp;
 
 	/*     GENERATING UPPER TRIANGULAR SCRMABLING MATRICES AND */
@@ -543,23 +409,19 @@ L30:
 	usm -= 32;
 
 	/* Function Body */
-	i__1 = sobol_2.maxcol;
-	for (i__ = 1; i__ <= i__1; ++i__) {
+	for (i = 1; i <= ssobol_maxcol; ++i) {
 		stemp = (int) (uni_() * 1e3f) % 2;
-		ushift[i__] = stemp;
-		i__2 = sobol_2.maxcol;
-		for (j = 1; j <= i__2; ++j) {
-			if (j == i__) {
+		ushift[i] = stemp;
+		for (j = 1; j <= ssobol_maxcol; ++j) {
+			if (j == i) {
 				temp = 1;
-			} else if (j > i__) {
+			} else if (j > i) {
 				temp = (int) (uni_() * 1e3f) % 2;
 			} else {
 				temp = 0;
 			}
-			usm[i__ + j * 31] = temp;
-			/* L30: */
+			usm[i + j * 31] = temp;
 		}
-		/* L20: */
 	}
 	return 0;
 } /* genscrmu_ */
@@ -617,13 +479,10 @@ double uni_(void)
 	return output;
 }
 
-/* Subroutine */ int gossobl_(double *quasi)
+int ssobol_next(double *quasi)
 {
-	/* System generated locals */
-	int i__1;
-
 	/* Local variables */
-	static int i__, l;
+	static int i, l;
 
 
 	/*     THIS SUBROUTINE GENERATES A NEW */
@@ -661,44 +520,39 @@ double uni_(void)
 
 	/* Function Body */
 	l = 0;
-	i__ = sobol_1.count;
+	i = ssobol_count;
 L1:
 	++l;
-	if (i__ % 2 == 1) {
-		i__ /= 2;
+	if (i % 2 == 1) {
+		i /= 2;
 		goto L1;
 	}
 
 	/*     CHECK THAT THE USER IS NOT CHEATING ! */
 
-	if (l > sobol_1.maxcol) {
-		printf(" TOO MANY CALLS ON GOSOBL");
+	if (l > ssobol_maxcol) {
+		printf("ssobol_next : too many calls.");
 		exit(-1);
 	}
 
 	/*     CALCULATE THE NEW COMPONENTS OF QUASI, */
 	/*     FIRST THE NUMERATORS, THEN NORMALIZED */
 
-	i__1 = sobol_1.s;
-	for (i__ = 1; i__ <= i__1; ++i__) {
-		sobol_1.lastq[i__ - 1] = exor_(&sobol_1.lastq[i__ - 1], &sobol_1.sv[
-			i__ + l * 40 - 41]);
+	for (i = 1; i <= ssobol_s; ++i) 
+	{
+		ssobol_lastq[i - 1] = exor_(&ssobol_lastq[i - 1], &ssobol_sv[i-1][l-1]);
 
-			/*     IF A FULL-WORD EXCLUSIVE-OR, SAY .XOR., IS AVAILABLE */
-			/*     THEN REPLACE THE PRECEDING STATEMENT BY */
-
-			/*         LASTQ(I) = LASTQ(I) .XOR. SV(I,L) */
-
-			/*     TO GET A FASTER, EXTENDED FORTRAN PROGRAM */
-
-			quasi[i__] = sobol_1.lastq[i__ - 1] * sobol_1.recipd;
-			/* L2: */
+		/*     IF A FULL-WORD EXCLUSIVE-OR, SAY .XOR., IS AVAILABLE */
+		/*     THEN REPLACE THE PRECEDING STATEMENT BY */
+		/*         LASTQ(I) = LASTQ(I) .XOR. SV(I,L) */
+		/*     TO GET A FASTER, EXTENDED FORTRAN PROGRAM */
+		quasi[i] = ssobol_lastq[i - 1] * ssobol_recipd;
 	}
 
-	++sobol_1.count;
+	++ssobol_count;
 
 	return 0;
-} /* gossobl_ */
+}
 
 int exor_(int *iin, int *jin)
 {
@@ -706,19 +560,19 @@ int exor_(int *iin, int *jin)
 	int ret_val;
 
 	/* Local variables */
-	static int i__, j, k, l;
+	static int i, j, k, l;
 
 
 	/*     THIS FUNCTION CALCULATES THE EXCLUSIVE-OR OF ITS */
 	/*     TWO INPUT PARAMETERS */
 
-	i__ = *iin;
+	i = *iin;
 	j = *jin;
 	k = 0;
 	l = 1;
 
 L1:
-	if (i__ == j) {
+	if (i == j) {
 		ret_val = k;
 		return ret_val;
 	}
@@ -726,10 +580,10 @@ L1:
 	/*     CHECK THE CURRENT RIGHT-HAND BITS OF I AND J. */
 	/*     IF THEY DIFFER, SET THE APPROPRIATE BIT OF K. */
 
-	if (i__ % 2 != j % 2) {
+	if (i % 2 != j % 2) {
 		k += l;
 	}
-	i__ /= 2;
+	i /= 2;
 	j /= 2;
 	l <<= 1;
 	goto L1;
@@ -746,25 +600,13 @@ int lbit_bits(int a, int b, int len)
 	x >>= b;
 	y <<= len;
 	return (int)(x & ~y);
-	}
+}
 
-typedef long int flag;
-typedef long int ftnlen;
-typedef long int ftnint;
 
-/*external read, write*/
-typedef struct
-{       flag cierr;
-        ftnint ciunit;
-        flag ciend;
-        char *cifmt;
-        ftnint cirec;
-} cilist;
-/* Main program */ int main(void)
+int main(void)
 {
 	/* Local variables */
 	int i, j, sam, maxd;
-	int flag__[2];
 	int taus, iflag, dimen;
 	double quasi[40];
 	int atmost;
@@ -782,16 +624,16 @@ typedef struct
 
 	sam = 1;
 	maxd = 30;
-	dimen = 6;
-	iflag = 2;
+	dimen = 5;
+	iflag = 3;
 	atmost = 50;
 	printf("iflag=%d\n",iflag);
 	printf("atmost=%d\n",atmost);
 	printf("dimen=%d\n",dimen);
 	printf("maxd=%d\n",maxd);
-	inssobl_(flag__, &dimen, &atmost, &taus, quasi, &maxd, &iflag);
+	ssobol_startup(dimen, atmost, &taus, quasi, &maxd, &iflag);
 	for (i = 2; i <= atmost; i++) {
-		gossobl_(quasi);
+		ssobol_next(quasi);
 		printf("quasi(%d)=",i);
 		for (j= 0; j < dimen; j++) {
 			printf(" %f",quasi[j]);
@@ -799,6 +641,6 @@ typedef struct
 		printf("\n");
 	}
 	return 0;
-} /* MAIN__ */
+}
 
 
